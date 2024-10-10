2bca324152e430912d3a68ff170f6829c9be0911
Sofia Tolonen <so8375to-s@student.lu.se>
2024-10-10 09:33:00 +0200

    Step 3:changed variable names and types

--- HW6/main.cxx
+++ HW6/main.cxx
@@ -1,21 +1,23 @@
 #include <iostream>
 #include <string>
-int main(int var0, char *var1[]) {
-  if (var0 == 3) {
-    std::string var2{var1[0]};
-    auto var3{*(var1[1])};
-    auto var4{var2.size()};
-    auto var5{std::atoi(var1[2])};
-    auto var6{0};
-    auto var7{0};
-    std::string var8{var1[1]};
+int main(int nrOfArgs, char *args[]) {
+  bool has3Args = (nrOfArgs == 3);
+  if  (has3Args) { //if true it continues, if false code stoppes
+    std::string codeName{args[0]};  //codeName = var2
+    char firstCharSecArg{*(args[1])}; // firstCharSecArg = var3
+    std::size_t lenOfCodeName{codeName.size()}; //lenOfCodeName=var4
+    int valueOfThirdArg{std::atoi(args[2])}; //valueOfThirdArg = var5
+    int sumCharsSecArg{0}; // var6 
+    int index{0}; //var7
+    std::string secArg{args[1]}; //secArg=var8
+    
     while (true) {
-      var6 += var8[var7++];
-      if (var7 >= static_cast<int>(var8.size())) {
+      sumCharsSecArg += secArg[index++];
+      if (index >= static_cast<int>(secArg.size())) {
         break;
       }
     }
-    if ((var6 ^ var3 * 3) << (var4 & 0x1f) == var5) {
+    if ((sumCharsSecArg ^ firstCharSecArg * 3) << (lenOfCodeName & 0x1f) == valueOfThirdArg) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
