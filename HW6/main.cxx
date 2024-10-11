4a07829d9dceb9aa68fb3dc5ce9bea830bfef147
Sofia Tolonen <so8375to-s@student.lu.se>
2024-10-11 12:17:43 +0200

    Step 3: factoring out while loop

--- HW6/main.cxx
+++ HW6/main.cxx
@@ -1,26 +1,31 @@
 #include <iostream>
 #include <string>
+
+int computeChecksum(const std::string& secArg) {
+    int checksum = 0;
+    for (char characters : secArg) {
+        checksum += characters; 
+    }
+    return checksum;
+}
+
 int main(int nrOfArgs, char *args[]) {
   bool has3Args = (nrOfArgs == 3);
-  if  (has3Args) { //if true it continues, if false code stoppes
-    std::string codeName{args[0]};  //codeName = var2
-    char firstCharSecArg{*(args[1])}; // firstCharSecArg = var3
-    std::size_t lenOfCodeName{codeName.size()}; //lenOfCodeName=var4
-    int valueOfThirdArg{std::atoi(args[2])}; //valueOfThirdArg = var5
-    int sumCharsSecArg{0}; // var6 
-    int index{0}; //var7
-    std::string secArg{args[1]}; //secArg=var8
+  if  (has3Args) { 
+    std::string codeName{args[0]};  //codeName before var2
+    char firstCharSecArg{*(args[1])}; // firstCharSecArg before var3
+    std::size_t lenOfCodeName{codeName.size()}; //lenOfCodeName  before var4
+    int valueOfThirdArg{std::atoi(args[2])}; //valueOfThirdArg before var5
+    std::string secArg{args[1]}; //secArg before var8
     
-    while (true) {
-      sumCharsSecArg += secArg[index++];
-      if (index >= static_cast<int>(secArg.size())) {
-        break;
-      }
-    }
+    int sumCharsSecArg = computeChecksum(secArg);
+
     if ((sumCharsSecArg ^ firstCharSecArg * 3) << (lenOfCodeName & 0x1f) == valueOfThirdArg) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
     }
-  }
-}
+  } 
+}  
+
+
