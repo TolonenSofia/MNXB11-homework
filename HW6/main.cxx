bbf30519df0002863aa0b16f3df236813d3695c1
Sofia Tolonen <so8375to-s@student.lu.se>
2024-10-11 15:12:23 +0200

    Step 4: Factoring out key calculation

--- HW6/main.cxx
+++ HW6/main.cxx
@@ -9,18 +9,23 @@ int computeChecksum(const std::string& secArg) {
     return checksum;
 }
 
+int calculateKey(int sumCharsSecArg, char firstCharSecArg, std::size_t lenOfCodeName) {
+    return (sumCharsSecArg ^ firstCharSecArg * 3) << (lenOfCodeName & 0x1f);
+}
+
 int main(int nrOfArgs, char *args[]) {
   bool has3Args = (nrOfArgs == 3);
   if  (has3Args) { 
-    std::string codeName{args[0]};  //codeName before var2
-    char firstCharSecArg{*(args[1])}; // firstCharSecArg before var3
-    std::size_t lenOfCodeName{codeName.size()}; //lenOfCodeName  before var4
-    int valueOfThirdArg{std::atoi(args[2])}; //valueOfThirdArg before var5
-    std::string secArg{args[1]}; //secArg before var8
-    
-    int sumCharsSecArg = computeChecksum(secArg);
+    std::string codeName{args[0]};  
+    char firstCharSecArg{*(args[1])}; 
+    std::size_t lenOfCodeName{codeName.size()}; 
+    int valueOfThirdArg{std::atoi(args[2])}; 
+    std::string secArg{args[1]}; 
 
-    if ((sumCharsSecArg ^ firstCharSecArg * 3) << (lenOfCodeName & 0x1f) == valueOfThirdArg) {
+    int sumCharsSecArg = computeChecksum(secArg);
+    int key = calculateKey(sumCharsSecArg, firstCharSecArg, lenOfCodeName);
+
+    if (key == valueOfThirdArg) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
