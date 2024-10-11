b248dde618f3e8480829fd7000f0f34f34268fa9
Sofia Tolonen <so8375to-s@student.lu.se>
2024-10-11 15:55:15 +0200

    Step 5: Added output for input error and results

--- HW6/main.cxx
+++ HW6/main.cxx
@@ -13,24 +13,38 @@ int calculateKey(int sumCharsSecArg, char firstCharSecArg, std::size_t lenOfCode
     return (sumCharsSecArg ^ firstCharSecArg * 3) << (lenOfCodeName & 0x1f);
 }
 
+void printingValues(int checksum, int key, int expectedKey) {
+  std::cout << "The checksum for the second argument is: " << checksum << std::endl;
+  std::cout << "The calculated key is: " << key << std::endl;
+  std::cout << "The expected key is: " << expectedKey << std::endl;
+}
+
+void printInputError(){
+  std::cout << "Wrong number of arguments, input 3 arguments!" << std::endl;
+  std::cout << "Example: <codeName> <secondArg> <thirdArg>" << std::endl;
+}
+
 int main(int nrOfArgs, char *args[]) {
   bool has3Args = (nrOfArgs == 3);
   if  (has3Args) { 
     std::string codeName{args[0]};  
     char firstCharSecArg{*(args[1])}; 
     std::size_t lenOfCodeName{codeName.size()}; 
-    int valueOfThirdArg{std::atoi(args[2])}; 
     std::string secArg{args[1]}; 
 
-    int sumCharsSecArg = computeChecksum(secArg);
+    int valueOfThirdArg{std::atoi(args[2])}; //called expected key in output
+    int sumCharsSecArg = computeChecksum(secArg); // called checksum in output
     int key = calculateKey(sumCharsSecArg, firstCharSecArg, lenOfCodeName);
+    printingValues(sumCharsSecArg, key, valueOfThirdArg);
 
     if (key == valueOfThirdArg) {
       std::cout << "Correct!" << std::endl;
     } else {
       std::cout << "Wrong!" << std::endl;
     }
-  } 
+  } else {
+    printInputError();
+    }
 }  
 
 
