50c7633047bdce69d41e521e92429dd1e8a2fcb7
Sofia Tolonen <so8375to-s@student.lu.se>
2024-11-05 16:45:06 +0100

    Added a second plot for the second part of task 3, px*py vs pz plot

--- HW8/read.cxx
+++ HW8/read.cxx
@@ -24,4 +24,21 @@ void read() {
     TCanvas *c1 = new TCanvas("c1", "Canvas", 800, 600);
     hist->Draw("COL");
     c1->Draw();    
+
+/////second plot
+
+    TH2F *hist2 = new TH2F("hist2", "px*py vs pz", 100, -0.003, 0.003, 100, -0.08, 0.08);
+
+    for (Int_t i = 0; i < N; i++) {
+        tree->GetEntry(i);
+        Double_t pxValue = magnitude->GetPx();
+        Double_t pyValue = magnitude->GetPy();
+        Double_t pzValue = magnitude->GetPz();
+        hist2->Fill(pxValue*pyValue, pzValue);
+    }
+
+    TCanvas *c2 = new TCanvas("c2", "Canvas", 800, 600);
+    hist2->Draw("COL");
+    c2->Draw();    
+
 }
